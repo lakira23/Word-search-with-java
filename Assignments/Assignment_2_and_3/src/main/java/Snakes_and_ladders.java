8c0da14de2607c3bee765d1538b8e12466651135
lakira23 <lakirar2020@gmail.com>
2026-02-12 22:24:03 +0000

    Implemented the swiiftbot input system and the current menu

--- Assignments/Assignment_2_and_3/src/main/java/Snakes_and_ladders.java
+++ Assignments/Assignment_2_and_3/src/main/java/Snakes_and_ladders.java
@@ -1,8 +1,5 @@
 //________imports______________
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
 
 import swiftbot.Button;
 import swiftbot.SwiftBotAPI;
@@ -12,94 +9,96 @@ import swiftbot.SwiftBotAPI;
 class Connectors {
 	int head;
 	int tail;
-	
+
 	public Connectors() {
-		
+
 	}
-	
+
 	private int assign_pos(ArrayList<Object> list_connectors) {
 		return 0;
 	}
-	
+
 	public int get_head() {
 		return this.head;
 	}
-	
+
 	public int get_tail() {
 		return this.tail;
 	}
 }
 
 class Snakes extends Connectors {
-	
+
 	public Snakes() {
-		
+
 	}
 }
 
 class Ladders extends Connectors {
 	public Ladders() {
-		
+
 	}
 }
 
 class Users {
 	int pos;
 	public Users() {
-		
+
 	}
-	
+
 	public int get_pos() {
 		return this.pos;
 	}
 }
 
-class Input_Handler{
-	
+public class Snakes_and_ladders {
+	static boolean game_status = true;
+	static String current_button = "";
 	static SwiftBotAPI swiftBot = SwiftBotAPI.INSTANCE;
-	public Input_Handler() {
+
+	private static void menu() {
+		System.out.println("Press [Y] in the SwiftBot to start the game! ");
+
+		while (true) {
+			input();
+
+			if (current_button == "Y") {
+				System.out.println("");
+				System.out.println("Welcome to Snakes and Ladders!");
+				break;
+			}
+		}	
+	}
+
+	public static void input() {
 		try {
 			swiftBot.disableButton(Button.A);
 			swiftBot.disableButton(Button.B);
 			swiftBot.disableButton(Button.X);
 			swiftBot.disableButton(Button.Y);
 
-			swiftBot.enableButton(Button.A, () -> {userinput.add("Red"); System.out.println("A been pressed");});
-			swiftBot.enableButton(Button.B, () -> {userinput.add("Blue"); System.out.println("B been pressed");});
-			swiftBot.enableButton(Button.X, () -> {userinput.add("Green"); System.out.println("X been pressed");});
-			swiftBot.enableButton(Button.Y, () -> {userinput.add("White"); System.out.println("Y been pressed");});
+			swiftBot.enableButton(Button.A, () -> {System.out.println(" ");System.out.println("A been pressed"); current_button = "A";});
+			swiftBot.enableButton(Button.B, () -> {System.out.println(" ");System.out.println("B been pressed"); current_button = "B";});
+			swiftBot.enableButton(Button.X, () -> {System.out.println(" ");System.out.println("X been pressed"); current_button = "X";});
+			swiftBot.enableButton(Button.Y, () -> {System.out.println(" ");System.out.println("Y been pressed"); current_button = "Y";});
 
 
 		}
 
 		catch (Exception e) {
 			System.out.println("please enter a correct key!!");
-			
+
 		}
-		
 	}
-}
-
-public class Snakes_and_ladders {
-	static boolean game_status = true;
- 
-	
-
 	public static void main(String[] args) {
-		
-		Map<String,Boolean> game_screen = new HashMap<>();
-		
-		game_screen.put("Menu", true);
-		game_screen.put("Game", false);
-//		System.out.println("Press [Y] in the SwiftBot to start");
-//		
-//		//System.out.println("test");
-		
+
+		menu();
 		while (game_status) {
-			
+			//game
 		}
 	}
-	
-	
-
 }
+
+
+
+
