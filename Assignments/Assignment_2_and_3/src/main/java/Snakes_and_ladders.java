3f0d5acf309434211ce58a59c4447bb9d337b394
lakira23 <lakirar2020@gmail.com>
2026-02-20 20:30:10 +0000

    logic for the connectors and polymorphism

--- Assignments/Assignment_2_and_3/src/main/java/Snakes_and_ladders.java
+++ Assignments/Assignment_2_and_3/src/main/java/Snakes_and_ladders.java
@@ -2,6 +2,7 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 import java.util.Scanner;
 
 import swiftbot.Button;
@@ -10,15 +11,61 @@ import swiftbot.SwiftBotAPI;
 
 //________CLASSES_______________
 class Connectors {
-	private int head;
-	private int tail;
+	protected int head;
+	protected int tail;
+	static Random random = new Random();
 
-	public Connectors() {
-
+	public Connectors(ArrayList<Connectors> list_connectors) {
+		assign_pos(list_connectors);
+	}
+	private static boolean pos_taken(int value, ArrayList<Connectors> list_connectors) {
+		for (Connectors c : list_connectors) {
+			if (c.get_head() == value || c.get_tail() == value) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	protected boolean check_head_and_tail_pos(int tail) {
+		return this.head > tail; //default is snake
 	}
 
-	private int assign_pos(ArrayList<Object> list_connectors) {
-		return 0;
+	private int generate_head(ArrayList<Connectors> list_connectors) {
+		
+		int head_min = 6;
+		int head_max = 24;
+
+		while (true) {
+			head = random.nextInt(head_max - head_min + 1) + head_min;
+
+			if (!pos_taken(head, list_connectors)) {
+				return head;
+			}
+		}
+	}
+
+	private int generate_tail(ArrayList<Connectors> list_connectors) {
+		int tail_min = 2;
+		int tail_max = 20;
+		
+		int headrow = (this.head - 1) / 5;
+		
+		while (true) {
+			tail = random.nextInt(tail_max - tail_min + 1) + tail_min;
+			int tailrow = (this.tail - 1) / 5;
+			
+			if (!pos_taken(this.tail, list_connectors) && (tailrow != headrow) && (check_head_and_tail_pos(tail))){
+				return tail;
+			}
+		}
+		
+	}
+	
+	private void assign_pos(ArrayList<Connectors> list_connectors) {
+		this.head = generate_head(list_connectors);
+		this.tail = generate_tail(list_connectors);
+		
 	}
 
 	public int get_head() {
@@ -32,14 +79,24 @@ class Connectors {
 
 class Snakes extends Connectors {
 
-	public Snakes() {
-
+	public Snakes(ArrayList<Connectors> list_connectors) {
+		super(list_connectors);
+	}
+	
+	@Override
+	protected boolean check_head_and_tail_pos(int tail) {
+		return this.head > tail; 
 	}
 }
 
 class Ladders extends Connectors {
-	public Ladders() {
-
+	public Ladders(ArrayList<Connectors> list_connectors) {
+		super(list_connectors);
+	}
+	
+	@Override
+	protected boolean check_head_and_tail_pos(int tail) {
+		return tail > this.head ; 
 	}
 }
 
@@ -55,13 +112,23 @@ class Users {
 }
 
 public class Snakes_and_ladders {
+
+	static ArrayList<Snakes> snakes_obj;
+	static ArrayList<Ladders> ladders_obj;
+	static ArrayList<Connectors> connectors_obj;
+
 	static String current_button = "";
 	static SwiftBotAPI swiftBot = SwiftBotAPI.INSTANCE;
 	static HashMap<String,Boolean> current_screen = new HashMap<>(Map.of(
 			"Menu", true,
 			"Game", false));
 	static String user_name;
-	
+	static int [][] board = {	
+			{21,22,23,24,25},
+			{20,19,18,17,16},
+			{11,12,13,14,15},
+			{10,9,8,7,6},
+			{1,2,3,4,5}}; 
 
 	private static void player_setup() {
 		Scanner text = new Scanner(System.in);
@@ -70,9 +137,14 @@ public class Snakes_and_ladders {
 		System.out.println("");
 		System.out.println("The SwiftBot has been assigned the following name:");
 		System.out.println("> SwiftBot");
+		text.close();
 	}
 
 	private static void Board_setup() {
+		snakes_obj = new ArrayList<Snakes>();
+		ladders_obj = new ArrayList<Ladders>();
+		connectors_obj = new ArrayList<Connectors>();
+
 
 	}
 
@@ -92,7 +164,7 @@ public class Snakes_and_ladders {
 		while (current_screen.get("Menu")) {
 			input();
 
-			if (current_button == "Y") {
+			if (current_button.equals("Y")) {
 				System.out.println("");
 				System.out.println("Welcome to Snakes and Ladders!");
 				current_screen.replace("Menu", false);
