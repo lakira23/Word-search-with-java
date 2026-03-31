6cd9685b859a590cda13811d2e1da26e7bcce45b
lakira23 <lakirar2020@gmail.com>
2026-03-31 14:09:03 +0100

    start on question 1

--- Assignments/Programming_application_task/src/word_search.java
+++ Assignments/Programming_application_task/src/word_search.java
@@ -5,5 +5,18 @@ public class word_search {
 		System.out.println("test2");
 
 	}
+	
+	public static int valid_sequence(String word) {
+		int flag = 1;
+		for (int i = 0; i < word.length(); i++) {
+			String current_letter = String.valueOf(word.charAt(i));
+			if (current_letter != current_letter.toUpperCase()) {
+				//the letter is not capital
+				flag = 0;
+			}
+		}
+		
+		return flag;
+	}
 
 }
